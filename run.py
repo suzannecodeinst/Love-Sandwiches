c6c2245f2b46c10be7adf8b435c543b03430fef9
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-09-18 17:31:15 +0000

    update surplus data on worksheet

--- run.py
+++ run.py
@@ -81,7 +81,16 @@ def calculate_surplus_data(sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
 
-    return surplus_data    
+    return surplus_data   
+
+def update_surplus_worksheet(data):
+    """
+    update surplus data worksheet, add new row with list data provided.
+    """
+    print("updating surplus worksheet..\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("surplus worksheet updated successfully.\n")     
 
 
 
@@ -93,7 +102,7 @@ def main():
     sales_data = [int(num) for num in data]  
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation.")
 main()    
